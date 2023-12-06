bdc7bf8eb8408a78bc0a154bab5463d17070574c
hunorkelemen <hunorkelemen52@gmail.com>
2023-12-06 17:32:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import Login from './component/Login'
+import Register from './component/Register'
 import './App.css';
 
 function App() {
+const [currentPage, setCurrentPage] = useState('login')
+
+const switchPage = (formPage) =>{
+  setCurrentPage(formPage)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {currentPage === 'login' ? <Login onFormSwitch={switchPage}/> : <Register onFormSwitch={switchPage}/>}
     </div>
-  );
+  ); 
 }
 
 export default App;
